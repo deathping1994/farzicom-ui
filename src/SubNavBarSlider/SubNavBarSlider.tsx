013eb7e913215b261b6fe83e201a5108aa4e5a86
HiteshDatt <hdk.hitesh26@gmail.com>
2023-03-13 18:37:29 +0530

    test

--- src/SubNavBarSlider/SubNavBarSlider.tsx
+++ src/SubNavBarSlider/SubNavBarSlider.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Image from "next/image";
-// import * as S from "./style";
-// import Link from "next/link";
+import Image from "next/image";
+import * as S from "./style";
+import Link from "next/link";
 
 export interface ISubNavBarSliderProps {
   sliderData: sliderDataInterface[];
-  sliderClass?: string;
+  sliderClass: string;
 }
 
 interface sliderDataInterface {
@@ -21,16 +21,34 @@ interface sliderDataInterface {
 
 export const SubNavBarSlider: React.FC<ISubNavBarSliderProps> = ({
   sliderData,
-  sliderClass = "subNavUI",
+  sliderClass,
 }) => {
   if (Array.isArray(sliderData) && sliderData.length > 0) {
     return (
-      <div className={`${sliderClass}__wrapperrrrrr`}>
-        test
-      </div>
+      <S.NavbarContainerWrapper className={`${sliderClass}__wrapper`}>
+        <S.SubNavOrderList
+          className={`${sliderClass}__lists`}
+          repeat={sliderData.length}
+        >
+          {sliderData?.map((navItems: sliderDataInterface) => (
+            <S.SubNavlist>
+              <Link href={navItems?.navigation || "/"}>
+                <Image
+                  src={navItems?.image || ""}
+                  alt={navItems?.alt}
+                  height={navItems?.imageDimensions?.height}
+                  width={navItems?.imageDimensions?.width}
+                  className={`${sliderClass}__image`}
+                />
+              </Link>
+              <p className={`${sliderClass}__title`}>{navItems?.title}</p>
+            </S.SubNavlist>
+          ))}
+        </S.SubNavOrderList>
+      </S.NavbarContainerWrapper>
     );
   }
-  return <></>;
+  return <></>
 };
 SubNavBarSlider.displayName = "SubNavBarSlider";
 export default SubNavBarSlider;
